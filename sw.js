11a378f5d38d130c63d27ec4a9f6aa61f7210785
feipla <feipla@users.noreply.github.com>
2026-05-17 13:40:27 +0000

    chore: 更新SW缓存版本v7，强制刷新前后台

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fitness-tracker-v6';
+const CACHE_NAME = 'fitness-tracker-v7';
 const ASSETS = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', (event) => {
