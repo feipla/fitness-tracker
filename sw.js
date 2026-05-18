3b8c1abb75afebbbca52f71356d6a780fa269e78
feipla <feipla@users.noreply.github.com>
2026-05-18 14:45:11 +0000

    fix: 添加上传分析进度提示，优化用户反馈

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fitness-tracker-v14';
+const CACHE_NAME = 'fitness-tracker-v15';
 const ASSETS = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', (event) => {
