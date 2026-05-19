356496d0a8e1066802cb9ba91478537379a09b5f
feipla <feipla@users.noreply.github.com>
2026-05-19 14:41:45 +0000

    feat: 海报式品牌登录页 - 沉浸式视觉+品牌故事+心率波形动效

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fitness-tracker-v34';
+const CACHE_NAME = 'fitness-tracker-v35';
 const ASSETS = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', (event) => {
