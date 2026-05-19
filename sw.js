fc5a018d32011f8a1028538fa4a607661320c7f0
feipla <feipla@users.noreply.github.com>
2026-05-19 14:04:07 +0000

    feat: 打卡提示移到用户卡片右侧，保留手臂图标

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fitness-tracker-v33';
+const CACHE_NAME = 'fitness-tracker-v34';
 const ASSETS = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', (event) => {
