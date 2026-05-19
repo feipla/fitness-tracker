636dd75472bceefe9884ea54bdcf428bd13694f4
feipla <feipla@users.noreply.github.com>
2026-05-19 07:50:27 +0000

    fix: 登录页改为fixed定位，防止主页内容露出

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fitness-tracker-v17';
+const CACHE_NAME = 'fitness-tracker-v18';
 const ASSETS = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', (event) => {
