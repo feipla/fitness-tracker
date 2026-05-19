a03466541ee161c15039c66bd18f5372c3b4957c
feipla <feipla@users.noreply.github.com>
2026-05-19 14:57:13 +0000

    feat: 全局深色沉浸式品牌视觉 - 所有页面统一风格

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fitness-tracker-v35';
+const CACHE_NAME = 'fitness-tracker-v36';
 const ASSETS = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', (event) => {
