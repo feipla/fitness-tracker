df89157eaa7ca9090bb38b418531ddc63b9a4d6a
feipla <feipla@users.noreply.github.com>
2026-05-19 10:13:08 +0000

    fix: 移除用户卡片中无意义的运动评分

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fitness-tracker-v20';
+const CACHE_NAME = 'fitness-tracker-v21';
 const ASSETS = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', (event) => {
