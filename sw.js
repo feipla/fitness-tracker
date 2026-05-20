a8137383b19799a35cb6584774a3db68278f555b
feipla <feipla@users.noreply.github.com>
2026-05-20 11:18:48 +0000

    feat: 全新自然有机风格设计 - OKLCH色彩+地图纹理+衬线字体

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fitness-tracker-v36';
+const CACHE_NAME = 'fitness-tracker-v37';
 const ASSETS = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', (event) => {
