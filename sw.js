b1e470c997c19b8d2d06ec1da8e6d39894919e36
feipla <feipla@users.noreply.github.com>
2026-05-20 14:23:59 +0000

    feat: 统计页面仪表盘风格 - 半圆弧形仪表+彩虹渐变+趋势图

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fitness-tracker-v40';
+const CACHE_NAME = 'fitness-tracker-v41';
 const ASSETS = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', (event) => {
