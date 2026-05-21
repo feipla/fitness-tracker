ffe90175f7c9e16444358dccaea9f97fd4639692
feipla <feipla@users.noreply.github.com>
2026-05-21 07:40:48 +0000

    feat: PWA独立图标 - 优化manifest配置+生成图标文件

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fitness-tracker-v44';
+const CACHE_NAME = 'fitness-tracker-v45';
 const ASSETS = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', (event) => {
