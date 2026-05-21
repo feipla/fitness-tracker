c6619b5d88402ba3023eaea225bc9c3ba3c1f78d
feipla <feipla@users.noreply.github.com>
2026-05-21 09:51:42 +0000

    feat: 添加PWA安装提示条 - 支持所有浏览器手动安装

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fitness-tracker-v45';
+const CACHE_NAME = 'fitness-tracker-v46';
 const ASSETS = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', (event) => {
