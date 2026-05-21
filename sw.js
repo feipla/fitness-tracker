eeb4ebfc59eaeb94a49e00ee74fa8e160792545b
feipla <feipla@users.noreply.github.com>
2026-05-21 10:35:59 +0000

    feat: 添加apple-touch-icon+各浏览器安装教程引导

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fitness-tracker-v46';
+const CACHE_NAME = 'fitness-tracker-v47';
 const ASSETS = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', (event) => {
