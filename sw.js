5b467d30e775b6a25ade476061bee287a7b4b1bd
feipla <feipla@users.noreply.github.com>
2026-05-22 00:59:23 +0000

    style: 优化首页卡片样式 - 增大字体、提升对比度、改善配色

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fitness-tracker-v52';
+const CACHE_NAME = 'fitness-tracker-v53';
 const ASSETS = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', (event) => {
