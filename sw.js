4f247757d72853b93c3eb7826a3b9d764c9fc1b3
feipla <feipla@users.noreply.github.com>
2026-05-22 11:10:10 +0000

    fix: 轨迹列表按时间降序排列，同一天按创建时间排序

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fitness-tracker-v55';
+const CACHE_NAME = 'fitness-tracker-v57';
 const ASSETS = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', (event) => {
