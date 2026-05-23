c75cd4aa5becc294ebf3c4c278603e089131bf4c
feipla <feipla@users.noreply.github.com>
2026-05-23 01:44:37 +0000

    style: 评估结果可视化 - 环形表盘+进度条+仪表盘动效

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fitness-tracker-v60';
+const CACHE_NAME = 'fitness-tracker-v61';
 const ASSETS = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', (event) => {
