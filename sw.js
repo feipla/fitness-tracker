a247e4e2ef1d25b2524bb1e426b973844e8123a7
feipla <feipla@users.noreply.github.com>
2026-05-23 05:39:20 +0000

    debug: 添加地图页数据调试日志

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fitness-tracker-v63';
+const CACHE_NAME = 'fitness-tracker-v64';
 const ASSETS = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', (event) => {
